9cfc092bf6d13b45d42f042da344b83b1f5f45f8
Ruiqi Gao <ruiqigao@princeton.edu>
2023-09-27 10:09:49 -0700

    debug

--- examples/train.py
+++ examples/train.py
@@ -1,3 +1,4 @@
+import jax.numpy as jnp
 import numpy as np
 from jax import jit, random, tree_util
 import jax, optax
@@ -19,15 +20,15 @@ if precision == '64':
     jax.config.update('jax_enable_x64', True)
 
 # DP config parameters
-save_name      = 'model_polaron.pkl' # trained model will be saved to this file
-train_data     = SingleDataSystem(['polaron_data'], ['coord', 'box', 'force', 'energy'])
-use_val_data   = True # if False, comment next line
-val_data       = SingleDataSystem(['polaron_data'], ['coord', 'box', 'force', 'energy'])
-orthorhombic   = True
+save_name      = 'model_polaron.pkl' # model save name
+train_data     = SingleDataSystem(['../data_example/water_test_data/6'], ['coord', 'box', 'force', 'energy'])
+# train_data     = SingleDataSystem(['../data_example/polaron_data'], ['coord', 'box', 'force', 'energy'])
+use_val_data   = False # if False, comment next line
+# val_data       = SingleDataSystem(['data_example/polaron_data'], ['coord', 'box', 'force', 'energy'])
 rcut           = 6.0
-embed_widths   = [16, 32, 64]
-fit_widths     = [64, 64, 64]
-axis_neuron    = 16
+embed_widths   = [24, 48, 96]
+fit_widths     = [240, 240, 240]
+axis_neuron    = 12
 batch_size     = 1
 val_batch_size = 8
 lr             = 0.002 
@@ -54,19 +55,23 @@ model = DPModel({'embed_widths':embed_widths,
                  'axis_neuron':axis_neuron,
                  'Ebias':train_data.compute_Ebias()})
 batch, lattice_args = train_data.get_batch(getstat_bs)
-static_args = nn.FrozenDict({'lattice': lattice_args | {'ortho':orthorhombic},
+static_args = nn.FrozenDict({'lattice': lattice_args,
                             'rcut':rcut,
                             'type_index':tuple(train_data.type_index),
                             'ntype_index':tuple(lattice_args['lattice_max']*train_data.type_index)})
 model.get_stats(batch['coord'], batch['box'], static_args)
+print('Model statistics computed.')
 variables = model.init(random.PRNGKey(RANDOM_SEED), batch['coord'][0], batch['box'][0], static_args)
+print('Model initialized with', sum(i.size for i in tree_util.tree_flatten(variables)[0]), 'parameters.')
 lr_scheduler = optax.exponential_decay(init_value=lr, transition_steps=decay_steps,
                     decay_rate=decay_rate, transition_begin=0, staircase=True)
 optimizer = optax.adam(learning_rate=lr_scheduler, b2=beta2)
 opt_state = optimizer.init(variables)
 loss, loss_and_grad = model.get_loss_ef_fn()
-print('System initialized with', sum(i.size for i in tree_util.tree_flatten(variables)[0]), 'parameters.')
+print('Optimizer initialized. Training started.')
 
+
+state_args = {'le_avg':0., 'lf_avg':0., 'loss_avg':0., 'iteration':0}
 def train_step(batch, variables, opt_state, state_args, static_args):
     r = lr_scheduler(state_args['iteration']) / lr
     pref = {'e': s_pref_e*r + l_pref_e*(1-r), 'f': s_pref_f*r + l_pref_f*(1-r)}
@@ -87,7 +92,6 @@ def val_step(batch, variables, static_args):
 if use_val_data:
     val_step = jit(val_step, static_argnums=(2,))
 
-state_args = {'le_avg':0., 'lf_avg':0., 'loss_avg':0., 'iteration':0}
 tic = time()
 for iteration in range(total_steps):
     batch, _ = train_data.get_batch(batch_size)
@@ -103,7 +107,7 @@ for iteration in range(total_steps):
               ' LF %7.5f' % (state_args['lf_avg']/beta)**0.5 + 
               (' LEval %7.5f' % ((loss_val_e)**0.5/val_data.natoms) if use_val_data else '') +
               (' LFval %7.5f' % (loss_val_f)**0.5 if use_val_data else '') +
-              ' Time %.2f' % (time()-tic))
+              ' Time %.2fs' % (time()-tic))
         tic = time()
 
 with open(save_name, 'wb') as file:
